46a9fe0aba3f7993aee3d8c88fd5b24e7b887f32
heiNina <nina.shabrova2001@gmail.com>
2020-07-18 21:02:26 +0300

    Masonry added to notes list

--- src/ui/List.js
+++ src/ui/List.js
@@ -2,13 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {NoteType} from '../models/note'
 import {MiniCard} from './MiniCard'
+import Masonry from 'react-masonry-component'
 import styles from './List.module.scss'
 
 
 export const List = ({notes}) => (
-    <ul className={styles.list}>{
-        notes.map(e => <li key={e.id}><MiniCard note={e}/></li>)
-    }</ul>
+    <Masonry elementType="ul" className={styles.list}>
+        {notes.map(e => (
+            <li key={e.id} className={styles.item}>
+                <MiniCard note={e}/>
+            </li>
+        ))}
+    </Masonry>
 )
 
 
