5abaf1a297a6b8a3594b47b0be498e7d1fa6a6b3
Toleckk <anatolich1995@gmail.com>
2020-07-20 02:40:37 +0300

    Store synchronization fixed

--- src/store/Persist.js
+++ src/store/Persist.js
@@ -1,11 +1,19 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {useNotes} from '../models/note'
 
 export const Persist = () => {
+    const [loaded, setLoaded] = useState(false)
     const {restoreNotes, saveNotes} = useNotes()
 
-    useEffect(restoreNotes, [restoreNotes])
-    useEffect(saveNotes, [saveNotes])
+    useEffect(() => {
+        restoreNotes()
+        setLoaded(true)
+    }, [restoreNotes])
+
+    useEffect(() => {
+        if(loaded)
+            saveNotes()
+    }, [saveNotes, loaded])
 
     return ''
 }
