93df0f8213cc4bf4700fe6b3b837be5672712e60
Toleckk <anatolich1995@gmail.com>
2020-07-19 21:59:56 +0300

    createNote function added to service

--- src/models/note/service.js
+++ src/models/note/service.js
@@ -5,7 +5,10 @@ import {nanoid} from 'nanoid'
 export const getNote = id => find(note => note.id === id)
 
 
-export const addNote = note => append({...note, id: nanoid()})
+export const createNote = note => ({...note, id: nanoid()})
+
+
+export const addNote = note => append(note.id ? note : createNote(note))
 
 
 export const replaceNote = note => map(e => e.id === note.id ? note : e)
