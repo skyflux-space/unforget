21366deeb9147b67607560f1efe8bd43f1a8903b
Toleckk <anatolich1995@gmail.com>
2020-07-19 20:20:27 +0300

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import {RecoilRoot} from 'recoil'
+import {useRoutes} from 'hookrouter'
+import {Dashboard} from './pages/Dashboard'
+import {Create} from './pages/Create'
 
+const routes = {
+    '/': () => <Dashboard/>,
+    '/create': () => <Create/>,
+}
 
 function App() {
-    return <React.StrictMode/>
+    const routeResult = useRoutes(routes)
+
+    return (
+        <React.StrictMode>
+            <RecoilRoot>
+                {routeResult || <div>Not Found</div>}
+            </RecoilRoot>
+        </React.StrictMode>
+    )
 }
 
 export default App
