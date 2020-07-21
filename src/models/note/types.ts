9bf0f577ec5d990398e5934c5bf9b107b966eb93
Toleckk <anatolich1995@gmail.com>
2020-07-21 18:02:36 +0300

    Note types added

---
+++ src/models/note/types.ts
@@ -0,0 +1,19 @@
+export interface ValidNote extends Note {
+    content: string | ContentList
+}
+
+export interface Note extends Identifiable {
+    title?: string
+    content?: string | ContentList
+}
+
+export interface Identifiable {
+    id: string
+}
+
+export type ContentList = ContentListItem[]
+
+export interface ContentListItem {
+    text: string
+    checked: boolean
+}
