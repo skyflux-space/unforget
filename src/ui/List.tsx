4e71b9a675b8de86f2176be5caeb89c86672385c
heiNina <nina.shabrova2001@gmail.com>
2020-07-24 22:32:07 +0300

    List interface changed

--- src/ui/List.tsx
+++ src/ui/List.tsx
@@ -1,27 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {NoteType} from '../models/note'
-import {ValidNote} from '../models/note/types'
-import {MiniCard} from './MiniCard'
+import React, {Children} from 'react'
 import Masonry from 'react-masonry-component'
 import styles from './List.module.scss'
 
 
-export type ListProps = {
-    notes: ValidNote[]
-}
-
-export const List: React.FC<ListProps> = ({notes}) => (
+export const List: React.FC = ({children}) => (
     <Masonry elementType="ul">
-        {notes.map(e => (
-            <li key={e.id} className={styles.item}>
-                <MiniCard note={e} selected={false}/>
+        {Children.map(children, (e, i) => (
+            <li key={i} className={styles.item}>
+                {e}
             </li>
         ))}
     </Masonry>
 )
-
-
-List.propTypes = {
-    notes: PropTypes.arrayOf(NoteType.isRequired).isRequired,
-}
