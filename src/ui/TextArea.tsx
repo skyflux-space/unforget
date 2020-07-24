c61f31a61e58970a026ad78088a59cd01716d173
heiNina <nina.shabrova2001@gmail.com>
2020-07-24 21:46:04 +0300

    withoutDefault prop added to TextArea

--- src/ui/TextArea.tsx
+++ src/ui/TextArea.tsx
@@ -7,23 +7,30 @@ import styles from './TextArea.module.scss'
 export type TextAreaProps = {
     size?: 'big' | 'small'
     fullSize?: boolean
+    withoutDefault?: boolean
 } & JSX.IntrinsicElements['textarea']
 
-export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({className, size, fullSize, ...props}, ref) => (
-    <textarea className={c(styles.textarea, styles[size!], fullSize && styles.full, className)} {...props} ref={ref}/>
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(({className, size, fullSize, withoutDefault, ...props}, ref) => (
+    <textarea className={c(
+        styles.textarea,
+        styles[size!],
+        fullSize && styles.full,
+        !withoutDefault && styles.default,
+        className,
+    )} {...props} ref={ref}/>
 ))
 
 TextArea.defaultProps = {
-    className: styles.default,
     size: 'big',
     fullSize: false,
+    withoutDefault: false,
 }
 
 TextArea.propTypes = {
-    className: PropTypes.string,
     size: PropTypes.oneOf([
         'big',
         'small',
     ]),
     fullSize: PropTypes.bool,
+    withoutDefault: PropTypes.bool,
 }
