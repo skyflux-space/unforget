c11d181d211b43cc0d1335c99dc5ead8a1b8b6b9
Toleckk <anatolich1995@gmail.com>
2020-07-25 04:24:52 +0300

    Note form logic extracted

---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,65 @@
+import React, {ChangeEventHandler, FocusEventHandler, useCallback, useEffect, useState} from 'react'
+import {useFieldArray, useForm} from 'react-hook-form'
+import {useDeepCompareEffect} from 'react-use'
+import {convertContentToList, convertContentToString, getContentType, useNote} from '../models/note'
+import {ContentListItem, Note} from '../models/note/types'
+import {CreateForm} from '../ui'
+
+
+export type NoteForm = {
+    id: string
+    readOnly?: boolean
+}
+
+export const NoteForm: React.FC<NoteForm> = ({id, readOnly}) => {
+    const {note, update} = useNote(id)
+    const [defaultValues, setDefaultValues] = useState<Partial<Note> | undefined>(undefined)
+
+    const {register, watch, control, reset} = useForm()
+    const {fields, append, remove} = useFieldArray<ContentListItem>({
+        control,
+        name: 'content',
+    })
+
+    useEffect(() => {
+        if(note && !defaultValues)
+            setDefaultValues(note)
+    }, [defaultValues, setDefaultValues, note])
+
+    useEffect(() => reset(defaultValues), [reset, defaultValues])
+
+    const data = watch()
+
+    useDeepCompareEffect(() => void (update(data)), [data])
+
+    const onTabSelect = useCallback((i: number) => {
+        const {content} = data
+        if (i === 0 && Array.isArray(content))
+            setDefaultValues({content: convertContentToString(content)})
+
+        if (i === 1 && typeof content === 'string')
+            setDefaultValues({content: convertContentToList(content)})
+    }, [setDefaultValues, data])
+
+    const createOnListFieldBlur = useCallback(
+        (i: number): FocusEventHandler<HTMLTextAreaElement> => ({target}) => !target.value && remove(i),
+        [remove],
+    )
+
+    const createFieldFromStaticFieldValue: ChangeEventHandler<HTMLTextAreaElement> = useCallback(
+        ({target}) => append({text: target.value}),
+        [append],
+    )
+
+    return (
+            <CreateForm
+                createRef={register}
+                createOnListFieldBlur={createOnListFieldBlur}
+                onListStaticInputChange={createFieldFromStaticFieldValue}
+                contentListFields={fields}
+                onTabSelect={onTabSelect}
+                type={getContentType(defaultValues?.content)}
+                readOnly={readOnly}
+            />
+    )
+}
