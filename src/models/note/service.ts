0448dc4be8bd6711552a32461399f9063b8e042e
Toleckk <anatolich1995@gmail.com>
2020-07-27 20:48:05 +0300

    Refactor

--- src/models/note/service.ts
+++ src/models/note/service.ts
@@ -1,4 +1,4 @@
-import {append, filter, find, map} from 'ramda'
+import {always, append, cond, eqProps, filter, find, identity, ifElse, isNil, map, reject, T, whereEq} from 'ramda'
 import {v4} from 'uuid'
 import {ContentList, ContentType, Identifiable, Note, ValidNote} from './types'
 
@@ -23,9 +23,10 @@ export const filterValidNotes
     : (list: Note[]) => ValidNote[]
     = filter(isValidNote) as (list: Note[]) => ValidNote[]
 
+
 export const getNote
     : (id: string) => (notes: Note[]) => Note | undefined
-    = id => find(note => note.id === id)
+    = id => find(whereEq({id}))
 
 
 export const createNote
@@ -33,19 +34,23 @@ export const createNote
     = () => ({id: v4()})
 
 
-export const addNote
-    : (note: Note) => (notes: Note[]) => Note[]
-    = append
+interface AddNote {
+    (note: Note): (notes: Note[]) => Note[]
+
+    (note: Note, notes: Note[]): Note[]
+}
+
+export const addNote: AddNote = append
 
 
 export const replaceNote
     : (note: Note) => (notes: Note[]) => Note[]
-    = note => map(e => e.id === note.id ? note : e)
+    = note => map(ifElse(eqProps('id', note), always(note), identity))
 
 
 export const removeNote
     : (id: string) => (notes: Note[]) => Note[]
-    = id => filter((note: Note) => note.id !== id)
+    = id => reject(whereEq({id}))
 
 
 export const saveNotes
@@ -53,10 +58,9 @@ export const saveNotes
     = notes => localStorage.setItem('notes', JSON.stringify(filterValidNotes(notes)))
 
 
-export const restoreNotes = (): Note[] => {
-    const data = localStorage.getItem('notes')
-    return data ? JSON.parse(data) : []
-}
+export const restoreNotes
+    : () => Note[]
+    = () => ifElse(isNil, always([]), JSON.parse)(localStorage.getItem('notes'))
 
 
 export const convertContentToString = (arr: ContentList) => arr.map(e => e.text).join('\n')
@@ -68,7 +72,10 @@ export const convertContentToList = (text?: string): ContentList => (text || '')
     .map(text => ({text, checked: false}))
 
 
-export const getContentType = (content?: string | ContentList): ContentType | undefined => {
-    if(content !== undefined)
-        return Array.isArray(content) ? ContentType.List : ContentType.String
-}
+export const getContentType
+    : (content?: string | ContentList) => ContentType | undefined
+    = cond([
+    [isNil, always(undefined)],
+    [Array.isArray, always(ContentType.List)],
+    [T, always(ContentType.String)]
+])
