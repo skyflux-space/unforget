d1d0bd3eaeca08b7cbb7265b1928060b9e552f7d
Toleckk <anatolich1995@gmail.com>
2020-07-29 01:36:37 +0300

    Dashboard select feature added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,27 +1,50 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import {A} from 'hookrouter'
-import {useNotes} from '../models/note'
-import {List, Button, BottomBar, MiniCard} from '../ui'
+import {Note} from '../models/note/types'
+import {addNote, removeNoteById, useNotes} from '../models/note'
+import {BottomBar, Button, List, MiniCard} from '../ui'
 
 
 export const Dashboard: React.FC = () => {
-    const {validNotes} = useNotes()
+    const {validNotes, removeNotes} = useNotes()
+
+    const [selectedNotes, selectNotes] = useState<Note[]>([])
+
+    const toggleSelect = useCallback((note: Note) => selectNotes(notes => {
+        if(!notes.includes(note))
+            return addNote(note, notes)
+
+        return removeNoteById(note.id, notes)
+    }), [selectNotes])
+
+    const removeSelected = useCallback(() => {
+        removeNotes(selectedNotes)
+        selectNotes([])
+    }, [removeNotes, selectedNotes, selectNotes])
 
     return (
         <main>
             <List>
                 {validNotes.map(note => (
-                    <A href={'/note/' + note.id}>
-                        <MiniCard note={note} selected={false}/>
+                    <A href={'/note/' + note.id} key={note.id}>
+                        <MiniCard
+                            note={note}
+                            selected={selectedNotes.includes(note)}
+                            onSelect={toggleSelect}
+                            isSelectMode={!!selectedNotes.length}
+                        />
                     </A>
                 ))}
             </List>
             <BottomBar
-                visible={false}
+                visible={!!selectedNotes.length}
                 button={
                     <A href="/create">
                         <Button round>+</Button>
                     </A>
+                }
+                left={
+                    <button onClick={removeSelected}>x</button>
                 }
             />
         </main>
