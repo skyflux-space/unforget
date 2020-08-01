9e99d2164c92705e6c706e0f3dab3d05757cfcd1
Toleckk <anatolich1995@gmail.com>
2020-08-01 21:54:23 +0300

    Pin handling added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,21 @@
-import React, {useCallback, useState} from 'react'
+import React, {useCallback, useMemo, useState} from 'react'
 import {A, navigate} from 'hookrouter'
-import {converge, ifElse, includes, map, pipe, unary} from 'ramda'
-import {Note} from '../models/note/types'
+import {converge, ifElse, includes, map, partition, pipe, propOr, unary} from 'ramda'
+import {Note, ValidNote} from '../models/note/types'
 import {addNote, removeNote, useNotes} from '../models/note'
 import {BottomBar, Button, List, MiniCard} from '../ui'
 
 
 const goToNote = (id: string) => () => navigate('/note/' + id)
 
-export const Dashboard: React.FC = () => {
-    const {validNotes, removeNotes} = useNotes()
+const partitionByPin: (note: ValidNote[]) => [ValidNote[], ValidNote[]] = partition(propOr(false, 'pinned'))
 
-    const [selectedNotes, selectNotes] = useState<Note[]>([])
+export const Dashboard: React.FC = () => {
+    const {validNotes, removeNotes, pinNotes, unpinNotes} = useNotes()
+
+    const sortedNotes = useMemo(() => partitionByPin(validNotes), [validNotes])
+
+    const [selectedNotes, selectNotes] = useState<ValidNote[]>([])
 
     const toggleSelect = useCallback<(note: Note) => void>(pipe(
         converge(ifElse, map(unary, [includes, removeNote, addNote])),
@@ -25,19 +29,36 @@ export const Dashboard: React.FC = () => {
         clearSelection()
     }, [removeNotes, selectedNotes, clearSelection])
 
+    const [pinnedSelected] = useMemo(() => partitionByPin(selectedNotes), [selectedNotes])
+
+    const pinSelected = () => {
+        pinNotes(selectedNotes)
+        clearSelection()
+    }
+
+    const unpinSelected = () => {
+        unpinNotes(selectedNotes)
+        clearSelection()
+    }
+
     return (
         <main>
-            <List>
-                {validNotes.map(note => (
-                    <MiniCard
-                        note={note}
-                        selected={selectedNotes.includes(note)}
-                        onSelect={() => toggleSelect(note)}
-                        onClick={selectedNotes.length ? () => toggleSelect(note) : goToNote(note.id)}
-                        key={note.id}
-                    />
-                ))}
-            </List>
+            {
+                sortedNotes.map((notes, i) => (
+                    <List key={i} pinned={notes.length > 0 && notes[0].pinned}>
+                        {notes.map(note => (
+                            <MiniCard
+                                note={note}
+                                selected={selectedNotes.includes(note)}
+                                onSelect={() => toggleSelect(note)}
+                                onClick={selectedNotes.length ? () => toggleSelect(note) : goToNote(note.id)}
+                                key={note.id}
+                                pinned={note.pinned}
+                            />
+                        ))}
+                    </List>
+                ))
+            }
             <BottomBar
                 visible={!!selectedNotes.length}
                 button={
@@ -47,9 +68,14 @@ export const Dashboard: React.FC = () => {
                 }
                 left={
                     <>
-                        <button onClick={clearSelection}>y</button>
+                        <button onClick={clearSelection}>c</button>
                         <button onClick={removeSelected}>x</button>
                     </>
+                }
+                right={
+                    <button onClick={pinnedSelected.length > 0 ? unpinSelected : pinSelected}>
+                        {pinnedSelected.length > 0 ? 'u' : 'p'}
+                    </button>
                 }
             />
         </main>
