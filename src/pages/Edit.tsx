0c23e93a52be69c95db330135e9d8e19495aa61a
Toleckk <anatolich1995@gmail.com>
2020-08-02 17:46:32 +0300

    Edit page header implemented

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useCallback, useMemo} from 'react'
 import {Header} from '../ui'
 import {NoteManager} from '../components/NoteManager'
+import {useNotes} from '../models/note'
+import {navigate} from 'hookrouter'
 
 
 export type EditProps = {
@@ -8,9 +10,24 @@ export type EditProps = {
 }
 
 export const Edit: React.FC<EditProps> = ({id}) => {
+    const {pinNote, unpinNote, getNote, removeNote} = useNotes()
+    const note = useMemo(() => getNote(id), [getNote, id])
+    const pin = useCallback(() => note && pinNote(note), [note, pinNote])
+    const unpin = useCallback(() => note && unpinNote(note), [note, unpinNote])
+    const remove = useCallback(() => {
+        if(note)
+            removeNote(note)
+        navigate('/', true)
+    }, [note, removeNote])
+
     return (
         <div style={{position: 'fixed', height: '100%', width: '100%', display: 'flex', flexDirection: 'column'}}>
-            <Header/>
+            {note &&
+                <Header>
+                    <button onClick={note.pinned ? unpin : pin}>{note.pinned ? 'Unpin' : 'Pin'}</button>
+                    <button onClick={remove}>Remove</button>
+                </Header>
+            }
             <NoteManager id={id}/>
         </div>
     )
