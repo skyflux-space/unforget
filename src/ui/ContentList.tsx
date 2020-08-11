71ea06ebef545e76f6450af193034c6c53b11fdf
heiNina <nina.shabrova2001@gmail.com>
2020-08-11 20:10:00 +0300

    Partition by checked added

--- src/ui/ContentList.tsx
+++ src/ui/ContentList.tsx
@@ -1,47 +1,60 @@
-import React, {ChangeEventHandler, FocusEventHandler, memo} from 'react'
-import {ContentListItem as ContentListItemType, Identifiable} from '../models/note/types'
+import React, {FocusEventHandler, memo, useMemo} from 'react'
+import {ContentList as ContentListType, partitionByChecked} from '../models/content'
 import {ContentListItem} from '../ui'
 import styles from './ContentList.module.scss'
 
 export type ContentListProps = {
-    fields: Partial<ContentListItemType & Identifiable>[]
+    fields: ContentListType
+    readOnly?: boolean
     createRef?: <T extends HTMLElement>() => React.Ref<T>
     onFieldBlur?: FocusEventHandler<HTMLTextAreaElement>
-    createOnFieldBlur?: (i: number) => FocusEventHandler<HTMLTextAreaElement>
-    onStaticInputChange?: ChangeEventHandler<HTMLTextAreaElement>
-    onFieldChecked?: (i: number, value: boolean) => void
+    onStaticInputChange?: (text: string) => void
     onFieldRemoved?: (i: number) => void
-    readOnly?: boolean
 }
 
 export const ContentList: React.FC<ContentListProps> = memo((
-    {fields, createRef, onFieldBlur, createOnFieldBlur, onStaticInputChange, readOnly, onFieldRemoved, onFieldChecked}
-    ) => (
-        <ul>
-            {fields.map((e, i) => (
-                <li key={e.id} className={styles.margin}>
-                    <ContentListItem
-                        onBlur={createOnFieldBlur?.(i) || onFieldBlur}
-                        onRemoveClicked={() => onFieldRemoved?.(i)}
-                        defaultText={e.text}
-                        checked={!!e.checked}
-                        ref={createRef?.()}
-                        name={`content[${i}]`}
-                        readOnly={readOnly}
-                        index={e.index}
-                        onChecked={(value: boolean) => onFieldChecked?.(i, value)}
-                    />
-                </li>
-            ))}
-            {!readOnly && (
+    {fields, createRef, onFieldBlur, onStaticInputChange, readOnly, onFieldRemoved},
+    ) => {
+        const [checked, unchecked] = useMemo(() => partitionByChecked(fields), [fields])
+
+        return (
+            <ul>
+                {unchecked.map((e, i) => (
+                    <li className={styles.margin} key={e.index.toString()}>
+                        <ContentListItem
+                            onBlur={onFieldBlur}
+                            onRemoveClicked={() => onFieldRemoved?.(i)}
+                            defaultText={e.text}
+                            checked={e.checked}
+                            ref={createRef?.()}
+                            name={`content[${i}]`}
+                            readOnly={readOnly}
+                            index={e.index}
+                        />
+                    </li>
+                ))}
                 <li className={styles.margin}>
                     <ContentListItem
                         disabled
-                        onTextChange={onStaticInputChange}
+                        onTextChange={({target: {value}}) => onStaticInputChange?.(value)}
                         checked={false}
                     />
                 </li>
-            )}
-        </ul>
-    )
+                {checked.map((e, i) => (
+                    <li className={styles.margin} key={e.index.toString()}>
+                        <ContentListItem
+                            onBlur={onFieldBlur}
+                            onRemoveClicked={() => onFieldRemoved?.(unchecked.length + i)}
+                            defaultText={e.text}
+                            checked={e.checked}
+                            ref={createRef?.()}
+                            name={`content[${unchecked.length + i}]`}
+                            readOnly={readOnly}
+                            index={e.index}
+                        />
+                    </li>
+                ))}
+            </ul>
+        )
+    },
 )
