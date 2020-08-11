fbaff10d99e6c9c0c87550ba24be0ecfada1f71b
heiNina <nina.shabrova2001@gmail.com>
2020-08-11 20:10:32 +0300

    Partition by checked added

--- src/ui/NoteForm.tsx
+++ src/ui/NoteForm.tsx
@@ -1,81 +1,92 @@
-import React, {ChangeEventHandler, FocusEventHandler} from 'react'
+import React, {FocusEventHandler, FormEventHandler, useCallback} from 'react'
 import {Tab, TabList, TabPanel, Tabs} from 'react-tabs'
 import c from 'classnames'
-import {ContentListItem as ContentListItemType, ContentType, Identifiable} from '../models/note/types'
+import {Note} from '../models/note'
+import {ContentList as ContentListType, ContentType, getContentType} from '../models/content'
 import {Input} from './Input'
 import {TextArea} from './TextArea'
 import {ContentList} from './ContentList'
 import styles from './NoteForm.module.scss'
+import {always, cond, equals, identity, ifElse, isNil, pipe} from 'ramda'
 
 
 export type CreateFormProps = {
-    contentListFields: Partial<ContentListItemType & Identifiable>[]
-    onTabSelect?: (i: number) => void
-    createRef?: <T extends HTMLElement>() => React.Ref<T>
-    onListStaticInputChange?: ChangeEventHandler<HTMLTextAreaElement>
-    onListFieldBlur?: FocusEventHandler<HTMLTextAreaElement>
-    createOnListFieldBlur?: (i: number) => FocusEventHandler<HTMLTextAreaElement>
-    onListFieldRemoved?: (i: number) => void
-    onListFieldChecked?: (i: number, value: boolean) => void
+    note: Note
     readOnly?: boolean
-    type?: ContentType
+    createRef?: <T extends HTMLElement>() => React.Ref<T>
+    onContentTypeChanged?: (type: ContentType) => void
+    onListStaticInputChange?: (text: string) => void
+    onListFieldBlur?: FocusEventHandler<HTMLTextAreaElement>
+    onListFieldRemoved?: (i: number) => void
+    onSubmit?: FormEventHandler<HTMLFormElement>
 }
 
 export const NoteForm: React.FC<CreateFormProps> = (
     {
-        contentListFields,
-        onTabSelect,
+        note,
+        readOnly = false,
         createRef,
+        onContentTypeChanged,
         onListStaticInputChange,
-        createOnListFieldBlur,
         onListFieldBlur,
         onListFieldRemoved,
-        onListFieldChecked,
-        readOnly = false,
-        type = ContentType.String,
-    }) => (
-    <form className={c(styles.flex, styles.column, styles.grow, styles.hidden)}>
-        <Input name="title" placeholder="Title..." ref={createRef} readOnly={readOnly}/>
-        <Tabs
-            className={c(styles.flex, styles.column, styles.grow)}
-            selectedTabPanelClassName={c(styles.grow, styles.scroll, styles.padding)}
-            onSelect={onTabSelect}
-            selectedIndex={type}
-        >
-            <TabPanel>
-                <TextArea
-                    name="content"
-                    placeholder="Note..."
-                    fullSize
-                    ref={createRef}
-                    withoutDefault
-                    readOnly={readOnly}
-                />
-            </TabPanel>
-            <TabPanel>
-                <ContentList
-                    fields={contentListFields}
-                    createRef={createRef}
-                    createOnFieldBlur={createOnListFieldBlur}
-                    onFieldBlur={onListFieldBlur}
-                    onFieldRemoved={onListFieldRemoved}
-                    onStaticInputChange={onListStaticInputChange}
-                    readOnly={readOnly}
-                    onFieldChecked={onListFieldChecked}
-                />
-            </TabPanel>
-            {
-                !readOnly
-                    ? (
-                        <div className={styles.flex}>
-                            <TabList className={c(styles.flex, styles.grow, styles.end)}>
-                                <Tab>Текст</Tab>
-                                <Tab>Список</Tab>
-                            </TabList>
-                        </div>
-                    ) : null
-            }
-        </Tabs>
-    </form>
-)
+        onSubmit,
+    }) => {
+
+    const onTabSelect: (i: number) => void = useCallback(ifElse(
+        pipe(always(onContentTypeChanged), isNil),
+        identity,
+        pipe(
+            cond<number, ContentType>([
+                [equals(0), always(ContentType.String)],
+                [equals(1), always(ContentType.List)],
+            ]),
+            onContentTypeChanged!,
+        ),
+    ), [onContentTypeChanged])
+
+    return (
+        <form onSubmit={onSubmit} className={c(styles.flex, styles.column, styles.grow, styles.hidden)}>
+            <Input name="title" placeholder="Title..." ref={createRef} readOnly={readOnly}/>
+            <Tabs
+                className={c(styles.flex, styles.column, styles.grow)}
+                selectedTabPanelClassName={c(styles.grow, styles.scroll, styles.padding)}
+                onSelect={onTabSelect}
+                selectedIndex={getContentType(note.content) || 0}
+            >
+                <TabPanel>
+                    <TextArea
+                        name="content"
+                        placeholder="Note..."
+                        fullSize
+                        ref={createRef}
+                        withoutDefault
+                        readOnly={readOnly}
+                    />
+                </TabPanel>
+                <TabPanel>
+                    <ContentList
+                        fields={note.content as ContentListType}
+                        createRef={createRef}
+                        onFieldBlur={onListFieldBlur}
+                        onFieldRemoved={onListFieldRemoved}
+                        onStaticInputChange={onListStaticInputChange}
+                        readOnly={readOnly}
+                    />
+                </TabPanel>
+                {
+                    !readOnly
+                        ? (
+                            <div className={styles.flex}>
+                                <TabList className={c(styles.flex, styles.grow, styles.end)}>
+                                    <Tab>Текст</Tab>
+                                    <Tab>Список</Tab>
+                                </TabList>
+                            </div>
+                        ) : null
+                }
+            </Tabs>
+        </form>
+    )
+}
 
